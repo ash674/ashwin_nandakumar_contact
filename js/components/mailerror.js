ebafb623f037eb756e1c8b48162f8216ef3ee99b
ashwin <ashu61998@gmail.com>
2020-11-29 21:29:48 +0530

    js and mailerror added

--- js/components/mailerror.js
+++ js/components/mailerror.js
@@ -1,8 +1,6 @@
 
 async function sentMail(target) {
    
-    let formData = new FormData(target);
-    // TODO: examine the method and incrementally build out the fetch call. currently it assumes POST
 
     let result = await fetch(`${target.getAttribute("action")}`, {
         method: target.method,
